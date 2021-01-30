866521b6a1115db25d89262a36a3ca6ddc9dd985
Seung <seungej@gmail.com>
2021-01-29 22:35:16 -0600

    Header

---
+++ linkedin-clone/src/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications"
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt=""/>
+
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text"/>
+                </div>
+            </div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C4E03AQG2Qmx1FZhV2g/profile-displayphoto-shrink_800_800/0/1610498325250?e=1617235200&v=beta&t=WB_cj_RSpvtXaVQChSFE29leiCOpYftLE11ey8UWhNc" title='me' />
+            </div>
+        </div>
+    )
+}
+
+export default Header
