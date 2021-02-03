bae5740ae189a9ce610f9d42ee62eaea4ccdc5d6
Seung <seungej@gmail.com>
2021-02-02 22:39:58 -0600

    Login

--- linkedin-clone/src/Login.js
+++ linkedin-clone/src/Login.js
@@ -14,7 +14,15 @@ function Login() {
                 <input
                     placeholder="Full name (required if registering)"
                     type="text"/>
+                <input placeholder="Profile pic URL (optional)" type="text"/>
+                <input placeholder="Email" type="email"/>
+                <input placeholder="Password" type="password"/>
+                <button>Sign In</button>
             </form>
+            <p>
+                Not a member?
+                <span>Register Now</span>
+            </p>
         </div>
     )
 }
